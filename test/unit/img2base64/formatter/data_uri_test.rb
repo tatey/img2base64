b20eca68844fda9fa010f1f821b2e8b4e23d32da
Tate Johnson <tate@tatey.com>
2012-12-30 09:44:30 +1100

    Change double quotes to single quotes.
    For consistency we use single quotes for literal strings.

--- test/unit/img2base64/formatter/data_uri_test.rb
+++ test/unit/img2base64/formatter/data_uri_test.rb
@@ -7,6 +7,6 @@ class Img2Base64::Formatter::DataURITest < MiniTest::Unit::TestCase
     file.expect :mime_type, 'image/jpg'
     file.expect :encode, 'ABC123=='
     formatter = Img2Base64::Formatter::DataURI.new file
-    assert_equal "data:image/jpg;base64,ABC123==", formatter.to_formatted_s
+    assert_equal 'data:image/jpg;base64,ABC123==', formatter.to_formatted_s
   end
 end
